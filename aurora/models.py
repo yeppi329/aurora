74a3e3fe1b6131af781ef8151c2051e557019b64
lia <lia@arbeon.com>
2023-09-25 11:31:32 +0900

    fix:DB테이블 수정 및 추가. 마이그레이션

--- aurora/models.py
+++ aurora/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class SummaryScanInfo(models.Model):
     scan_id = models.BigAutoField(primary_key=True)
     reg_date = models.CharField(max_length=255)
-    scan_success = models.BigIntegerField
-    scan_fail = models.BigIntegerField
-    scan_user = models.BigIntegerField
+    scan_success = models.BigIntegerField(default=0)
+    scan_fail = models.BigIntegerField(default=0)
+    scan_user = models.BigIntegerField(default=0)
 
     class Meta:
         db_table = 'summary_scan_info'  
@@ -16,17 +16,29 @@ class SummaryScanInfo(models.Model):
     
 class SummaryUserInfo(models.Model):
     summary_id = models.BigAutoField(primary_key=True)
-    summary_dt = models.BigIntegerField
-    total_user = models.BigIntegerField
-    new_user = models.BigIntegerField
-    suspended_user = models.BigIntegerField
-    deleted_user = models.BigIntegerField
-    return_user = models.BigIntegerField
-    inactive_user = models.BigIntegerField
-    dau = models.BigIntegerField
+    summary_dt = models.CharField(max_length=255)
+    total_user = models.BigIntegerField(default=0)
+    new_user = models.BigIntegerField(default=0)
+    suspended_user = models.BigIntegerField(default=0)
+    deleted_user = models.BigIntegerField(default=0)
+    return_user = models.BigIntegerField(default=0)
+    inactive_user = models.BigIntegerField(default=0)
+    dau = models.BigIntegerField(default=0)
 
     class Meta:
         db_table = 'summary_user_info'  
 
     def __str__(self):
         return f'Summary ID: {self.summary_id}, Date: {self.summary_dt}'
+       
+class SummaryContentInfo(models.Model):
+    summary_id = models.BigAutoField(primary_key=True)
+    reg_date = models.CharField(max_length=255)
+    content_type = models.CharField(max_length=255)
+    content_cnt = models.BigIntegerField(default=0) #스캔,등록건수
+    user_cnt = models.BigIntegerField(default=0)
+    class Meta:
+        db_table = 'summary_content_info'  
+
+    def __str__(self):
+        return f'Summary ID: {self.summary_id}, Date: {self.reg_date}'
