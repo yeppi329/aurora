1ebe080100406ad796581aeb52824025049f7e32
yeppi329 <yeppi329@naver.com>
2023-10-06 12:05:52 +0900

    feat:이용자 현황 페이지 DB연동 및 탭 개발

--- data/data_views.py
+++ data/data_views.py
@@ -1,15 +1,35 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
+from aurora.models import SummaryUserMonthInfo
+from aurora.models import SummaryUserDailyInfo
 
 @login_required(login_url='aurora:login')
 def index(request):
     context = {"page_title": "이용자 현황"}
-    return render(request, 'aurora/pages/data/svc-user-status.html', context)
+    return render(request, 'aurora/pages/data/svc-user-status-daily.html', context)
 
 @login_required(login_url='aurora:login')
-def svc_user_status(request):
-    context = {"page_title": "이용자 현황"}
-    return render(request, 'aurora/pages/data/svc-user-status.html', context)
+def svc_user_status_daily(request):
+    all_data = SummaryUserDailyInfo.objects.all()
+    for data in all_data:
+        print(f'Summary ID: {data.summary_id}, Date: {data.summary_dt}, Total Users: {data.total_user}, deleted_user: {data.deleted_user}')
+
+    context = {"page_title": "이용자 현황(일별)","all_data": all_data}
+    return render(request, 'aurora/pages/data/svc-user-status-daily.html', context)
+
+@login_required(login_url='aurora:login')
+def svc_user_status_month(request):
+    all_data = SummaryUserMonthInfo.objects.all()
+    for data in all_data:
+        print(f'Summary ID: {data.summary_id}, Date: {data.summary_dt}, Total Users: {data.total_user}, deleted_user: {data.deleted_user}')
+
+    context = {"page_title": "이용자 현황(월별)","all_data": all_data}
+    return render(request, 'aurora/pages/data/svc-user-status-month.html', context)
+
+@login_required(login_url='aurora:login')
+def svc_activity_user(request):
+    context = {"page_title": "기간별 이용자"}
+    return render(request, 'aurora/pages/data/svc-activity-user.html', context)
 
 @login_required(login_url='aurora:login')
 def scan_status(request):
--- data/urls.py
+++ data/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 app_name = 'data'
 
 urlpatterns = [
-    path("svc-user-status/", data_views.svc_user_status, name="svc-user-status"),
+    path("svc-user-status-daily/", data_views.svc_user_status_daily, name="svc-user-status-daily"),
+    path("svc-user-status-month/", data_views.svc_user_status_month, name="svc-user-status-month"),
+    path("svc-activity-user/", data_views.svc_activity_user, name="svc-activity-user"),
     path("scan-status/", data_views.scan_status, name="scan-status"),
 ]
