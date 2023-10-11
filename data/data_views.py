41d077581da6585271d81eb724bb315de54ae74b
yeppi329 <yeppi329@naver.com>
2023-10-11 15:03:57 +0900

    feat:스캔현황페이지 개발

--- data/data_views.py
+++ data/data_views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
-from aurora.models import SummaryUserMonthInfo
-from aurora.models import SummaryUserDailyInfo
+from aurora.models import SummaryUserMonthInfo,SummaryUserDailyInfo,SummaryUserPeriodInfo
+from aurora.models import SummaryScanDailyInfo,SummaryScanMonthInfo,SummaryScanHourInfo
 
 @login_required(login_url='aurora:login')
 def index(request):
@@ -29,10 +29,39 @@ def svc_user_status_month(request):
 @login_required(login_url='aurora:login')
 def svc_activity_user(request):
     context = {"page_title": "기간별 이용자"}
+    all_data = SummaryUserPeriodInfo.objects.all()
+    for data in all_data:
+        print(f'Summary ID: {data.summary_id}, Date: {data.summary_dt}, use_day_7: {data.use_day_7}')
+    
+    context = {"page_title": "기간별 이용자","all_data": all_data}
     return render(request, 'aurora/pages/data/svc-activity-user.html', context)
 
 @login_required(login_url='aurora:login')
-def scan_status(request):
-    context = {"page_title": "스캔 현황"}
-    return render(request, 'aurora/pages/data/scan-status.html', context)
+def scan_status_daily(request):
+    context = {"page_title": "스캔 현황(일별)"}
+    all_data = SummaryScanDailyInfo.objects.all()
+    for data in all_data:
+        print(f'Summary ID: {data.summary_id}, Date: {data.summary_dt}, scan_success: {data.scan_success}')
+    
+    context = {"page_title": "스캔 현황(일별)","all_data": all_data}
+    return render(request, 'aurora/pages/data/scan-status-daily.html', context)
 
+@login_required(login_url='aurora:login')
+def scan_status_month(request):
+    context = {"page_title": "스캔 현황(월별)"}
+    all_data = SummaryScanMonthInfo.objects.all()
+    for data in all_data:
+        print(f'Summary ID: {data.summary_id}, Date: {data.summary_dt}, scan_success: {data.scan_success}')
+    
+    context = {"page_title": "스캔 현황(월별)","all_data": all_data}
+    return render(request, 'aurora/pages/data/scan-status-month.html', context)
+
+@login_required(login_url='aurora:login')
+def scan_activity_status(request):
+    context = {"page_title": "스캔 현황(시간별)"}
+    all_data = SummaryScanHourInfo.objects.all()
+    for data in all_data:
+        print(f'Summary ID: {data.summary_id}, Date: {data.summary_dt}, scan_success: {data.scan_success}')
+    
+    context = {"page_title": "스캔 현황(시간별)","all_data": all_data}
+    return render(request, 'aurora/pages/data/scan-activity-status.html', context)
--- data/urls.py
+++ data/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path("svc-user-status-daily/", data_views.svc_user_status_daily, name="svc-user-status-daily"),
     path("svc-user-status-month/", data_views.svc_user_status_month, name="svc-user-status-month"),
     path("svc-activity-user/", data_views.svc_activity_user, name="svc-activity-user"),
-    path("scan-status/", data_views.scan_status, name="scan-status"),
+    path("scan-status-daily/", data_views.scan_status_daily, name="scan-status-daily"),
+    path("scan-status-month/", data_views.scan_status_month, name="scan-status-month"),
+    path("scan-activity-status/", data_views.scan_activity_status, name="scan-activity-status"),
+    #path("scan-status/", data_views.scan_status, name="scan-status"),
 ]
