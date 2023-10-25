cdb3822653b36ae5ed54d7e601a118b2ccbde858
riley <riley0422@icloud.com>
2023-10-25 11:40:10 +0900

    feat: 콘텐츠 상세 현황 관련 테이블 및 알비콘 리소스 정보 추가
    - 콘텐츠 상세 현황을 위한 테이블 추가
    - 알비콘 리소스 정보 추가

--- aurora/models.py
+++ aurora/models.py
@@ -196,3 +196,119 @@ class Category(models.Model):
     uri = models.CharField('카테고리 URI', max_length=255, blank=True)
     order = models.IntegerField('정렬순서', null=False, default=0)
     created_at = models.DateTimeField(auto_now=True, blank=True)
+
+# 콘텐츠 상세 현황 (미디어)
+class SummaryMediaDailyInfo(models.Model):
+    summary_id = models.BigAutoField(primary_key=True)
+    summary_dt = models.CharField(max_length=255, null=False, blank=False)     # 통계일 (YYYY-MM-DD)
+    content_type = models.SmallIntegerField(null=False, blank=False, default=0)
+    content_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    content_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    like_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    like_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    up_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    up_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    reply_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    reply_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    share_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    share_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now=True, blank=True)
+
+    class Meta:
+        db_table = "summary_media_daily_info"
+        unique_together = (("summary_dt", "content_type"),)
+
+class SummaryMediaMonthInfo(models.Model):
+    summary_id = models.BigAutoField(primary_key=True)
+    summary_dt = models.CharField(max_length=255, null=False, blank=False)     # 통계일 (YYYY-MM)
+    content_type = models.SmallIntegerField(null=False, blank=False, default=0)
+    content_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    content_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    like_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    like_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    up_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    up_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    reply_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    reply_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    share_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    share_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now=True, blank=True)
+
+    class Meta:
+        db_table = "summary_media_month_info"
+        unique_together = (("summary_dt", "content_type"),)
+
+# 콘텐츠 상세 현황 (알비콘)
+class SummaryArbeconDailyInfo(models.Model):
+    summary_id = models.BigAutoField(primary_key=True)
+    summary_dt = models.CharField(max_length=255, null=False, blank=False)     # 통계일 (YYYY-MM-DD)
+    resource_id = models.CharField(max_length=255, null=False, blank=False)
+    content_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    content_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    like_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    like_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    share_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    share_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now=True, blank=True)
+
+    class Meta:
+        db_table = "summary_arbecon_daily_info"
+        unique_together = (("summary_dt", "resource_id"),)
+
+class SummaryArbeconMonthInfo(models.Model):
+    summary_id = models.BigAutoField(primary_key=True)
+    summary_dt = models.CharField(max_length=255, null=False, blank=False)     # 통계일 (YYYY-MM)
+    resource_id = models.CharField(max_length=255, null=False, blank=False)
+    content_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    content_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    like_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    like_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    share_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    share_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now=True, blank=True)
+
+    class Meta:
+        db_table = "summary_arbecon_month_info"
+        unique_together = (("summary_dt", "resource_id"),)
+
+# 콘텐츠 상세 현황 (드로잉)
+class SummaryDrawingDailyInfo(models.Model):
+    summary_id = models.BigAutoField(primary_key=True)
+    summary_dt = models.CharField(max_length=255, null=False, blank=False)     # 통계일 (YYYY-MM-DD)
+    drawing_type = models.SmallIntegerField(null=False, blank=False, default=0)
+    content_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    content_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    like_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    like_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    share_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    share_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now=True, blank=True)
+
+    class Meta:
+        db_table = "summary_drawing_daily_info"
+        unique_together = (("summary_dt", "drawing_type"),)
+
+class SummaryDrawingMonthInfo(models.Model):
+    summary_id = models.BigAutoField(primary_key=True)
+    summary_dt = models.CharField(max_length=255, null=False, blank=False)     # 통계일 (YYYY-MM-DD)
+    drawing_type = models.SmallIntegerField(null=False, blank=False, default=0)
+    content_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    content_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    like_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    like_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    share_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    share_user_cnt = models.BigIntegerField(default=0, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now=True, blank=True)
+
+    class Meta:
+        db_table = "summary_drawing_month_info"
+        unique_together = (("summary_dt", "drawing_type"),)
+
+# 알비콘 리소스 코드
+class ArbeconResource(models.Model):
+    resource_id = models.CharField(primary_key=True, max_length=255, null=False, blank=False)
+    motion_name = models.CharField(max_length=255, null=False, blank=False)
+    show_name = models.CharField(max_length=255, null=False, blank=False)
+    resource_desc = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
